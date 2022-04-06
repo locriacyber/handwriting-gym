5028972888224103568e8586c39fb44b2ae3b49e
Locria Cyber <74560659+locriacyber@users.noreply.github.com>
2022-04-06 21:18:16 +0800

    Finish raylib demo

---
+++ build.zig
@@ -0,0 +1,44 @@
+const std = @import("std");
+// const glfw = @import("libs/glfw/build.zig");
+const raylib = @import("libs/raylib/lib.zig"); //call .Pkg() with the folder raylib-zig is in relative to project build.zig
+
+
+pub fn build(b: *std.build.Builder) void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
+    const target = b.standardTargetOptions(.{});
+
+    // Standard release options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
+    const mode = b.standardReleaseOptions();
+
+    const raylib_use_system = b.option(bool, "system-raylib", "link to preinstalled raylib libraries") orelse false;
+
+    const exe = b.addExecutable("handwriting-gym", "src/main.zig");
+    exe.setTarget(target);
+    exe.setBuildMode(mode);
+    exe.install();
+    // exe.addPackagePath("glfw", "libs/glfw/src/main.zig");
+    // glfw.link(b, exe, .{});
+    raylib.link(exe, raylib_use_system);
+    raylib.addAsPackage("raylib", exe);
+    raylib.math.addAsPackage("raylib-math", exe);
+
+    const run_cmd = exe.run();
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    const exe_tests = b.addTest("src/main.zig");
+    exe_tests.setTarget(target);
+    exe_tests.setBuildMode(mode);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&exe_tests.step);
+}
